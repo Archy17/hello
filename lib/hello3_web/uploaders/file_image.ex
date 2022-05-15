6ee09e93f35b1a7e7ead82b128397f102d215f37
Archy17 <1070711@gmail.com>
2022-05-16 00:16:06 +0300

    add USE WAFFLE.ecto

--- lib/hello3_web/uploaders/file_image.ex
+++ lib/hello3_web/uploaders/file_image.ex
@@ -1,5 +1,6 @@
 defmodule Hello3.FileImage do
   use Waffle.Definition
+  use Waffle.Ecto.Definition
 
   # Include ecto support (requires package waffle_ecto installed):
   # use Waffle.Ecto.Definition
